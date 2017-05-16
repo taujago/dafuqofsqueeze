84c1ff5ab035d0bc4bd35c9933d7a7ecc623f766
Onhacker <onh4cker@gmail.com>
2017-05-17 03:24:26 +0700

    surat pernyataan akta kelahiran

--- application/modules/surat_pernyataan_akta_kelahiran/controllers/surat_pernyataan_akta_kelahiran.php
+++ application/modules/surat_pernyataan_akta_kelahiran/controllers/surat_pernyataan_akta_kelahiran.php
@@ -91,7 +91,8 @@ class surat_pernyataan_akta_kelahiran extends op_controller {
             $responce->rows[$i]['nama']         = $result[$i]['nama']; 
             $responce->rows[$i]['alamat']       = $result[$i]['alamat'];     
             $responce->rows[$i]['tmp_lahir']  = $result[$i]['tmp_lahir'] ;             
-            $responce->rows[$i]['tgl_lahir']  = $result[$i]['tgl_lahir'] ;             
+            $responce->rows[$i]['tgl_lahir']  = $result[$i]['tgl_lahir'] ; 
+             $responce->rows[$i]['lengkap']  = $result[$i]['lengkap'] ;             
             
              $responce->rows[$i]['jk']          = $result[$i]['jk'] ; 
              $responce->rows[$i]['ttd']          = $result[$i]['ttd'] ; 
